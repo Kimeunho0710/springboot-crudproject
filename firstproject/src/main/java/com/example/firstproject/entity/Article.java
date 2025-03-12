2b5d751c2b53fd8e1fd703e9b263f29fd5c0886a
Kimeunho0710 <keh710427@gmail.com>
2025-03-12 16:46:25 +0900

    first commit

---
+++ firstproject/src/main/java/com/example/firstproject/entity/Article.java
@@ -0,0 +1,24 @@
+package com.example.firstproject.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String title;
+    @Column
+    private String content;
+
+
+}
